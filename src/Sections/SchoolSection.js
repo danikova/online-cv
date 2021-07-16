551c59e44562acbb679a769c3a083f8706a473fe
danikova <kovacs.daniel.9509@gmail.com>
2021-07-16 10:39:16 +0200

    Timeline refact and school timeline done

--- src/Sections/SchoolSection.js
+++ src/Sections/SchoolSection.js
@@ -2,53 +2,25 @@ import React from 'react';
 import Section from './Section';
 import SchoolIcon from '@material-ui/icons/School';
 
-import Timeline from '@material-ui/lab/Timeline';
-import TimelineItem from '@material-ui/lab/TimelineItem';
-import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
-import TimelineConnector from '@material-ui/lab/TimelineConnector';
-import TimelineContent from '@material-ui/lab/TimelineContent';
-import TimelineDot from '@material-ui/lab/TimelineDot';
-import TimelineOppositeContent from '@material-ui/lab/TimelineOppositeContent';
-
-import { Box, createStyles, makeStyles, Typography } from '@material-ui/core';
+import { Box, Typography } from '@material-ui/core';
 import { StaticRecursiveTreeView } from '../StaticRecursiveTreeView';
-
-export const useStyles = makeStyles((theme) =>
-    createStyles({
-        timelineRoot: {
-            margin: 0,
-            padding: 0,
-        },
-        topc: {
-            maxWidth: '100px',
-            paddingLeft: 0,
-        },
-    }),
-);
+import { Timeline, TimelineItem } from '../Timeline';
 
 const gdfSchoolData = [
     {
-        name: 'matematikai ismeretek',
-        children: [
-            { name: 'nulladrendű logikai ismeretek' },
-            { name: 'gráfelméleti ismeretek' },
-            { name: 'valószínűség számítási ismeretek' },
-        ],
-    },
-    {
         name: 'informatikai ismeretek',
         children: [
-            {
-                name: 'számítógéphálózati protokollok',
-            },
+            { name: 'SQL adatbázisok' },
+            { name: '.NET keretrendszer' },
+            { name: 'Windows / Linux szerverek üzemeltetése' },
+            { name: 'Mesterseges inteligenciai fogalmak' },
         ],
     },
     {
         name: 'programozasi ismeretek',
         children: [
-            { name: 'különböző adatszerkezetek részletes ismerete' },
-            { name: 'algoritmusok tervezése, implementálása' },
-            { name: 'statikus és dinamikus weboldalak készítése' },
+            { name: '.NET keretrendszer' },
+            { name: 'mobil alkalmazás fejlesztés' },
         ],
     },
 ];
@@ -57,9 +29,10 @@ const pazmanySchoolData = [
     {
         name: 'matematikai ismeretek',
         children: [
-            { name: 'nulladrendű logikai ismeretek' },
-            { name: 'gráfelméleti ismeretek' },
-            { name: 'valószínűség számítási ismeretek' },
+            { name: 'analízis' },
+            { name: 'nulladrendű logikai' },
+            { name: 'gráfelméleti' },
+            { name: 'valószínűség számítási' },
         ],
     },
     {
@@ -73,9 +46,9 @@ const pazmanySchoolData = [
     {
         name: 'programozasi ismeretek',
         children: [
-            { name: 'különböző adatszerkezetek részletes ismerete' },
+            { name: 'különböző adatszerkezetek részletes' },
             { name: 'algoritmusok tervezése, implementálása' },
-            { name: 'statikus és dinamikus weboldalak készítése' },
+            { name: 'dinamikus weboldalak készítése' },
         ],
     },
 ];
@@ -98,79 +71,49 @@ const petofiSchoolData = [
     },
 ];
 
-function ScoolTimelineItem({ year, content, last = false }) {
-    const classes = useStyles();
-
-    return (
-        <TimelineItem>
-            <TimelineOppositeContent className={classes.topc}>
-                <Typography color='textSecondary'>{year}</Typography>
-            </TimelineOppositeContent>
-            <TimelineSeparator>
-                <TimelineDot />
-                {!last && <TimelineConnector />}
-            </TimelineSeparator>
-            <TimelineContent>{content}</TimelineContent>
-        </TimelineItem>
-    );
-}
+const sumScoolData = [
+    {
+        year: '2019 - (2021)',
+        name: 'Gábor Dénes Főiskola (Távoktatás), Budapest (Magyarország)',
+        data: gdfSchoolData,
+    },
+    {
+        year: '2014 - 2019',
+        name: 'Pázmány Péter Katolikus Egyetem - Információs Technológiai Kar, Budapest (Magyarország)',
+        data: pazmanySchoolData,
+    },
+    {
+        year: '2011 - 2014',
+        name: 'Wattay Középiskola és Szakiskola, Kiskőrös (Magyarország)',
+        data: wattaySchoolData,
+    },
+    {
+        year: '2008 - 2011',
+        name: 'Kiskőrösi Petőfi Sándor Evangélikus Óvoda, Általános Iskola, Gimnázium és Kertészeti Szakközépiskola, Kiskőrös',
+        data: petofiSchoolData,
+    },
+];
 
 export function SchoolSection() {
-    const classes = useStyles();
-
     return (
         <Section icon={SchoolIcon} title='Tanulmányok'>
-            <Timeline className={classes.timelineRoot}>
-                <ScoolTimelineItem
-                    year={'2019 - (2021)'}
-                    content={
-                        <Box>
-                            <Typography>
-                                Gábor Dénes Főiskola (Távoktatás), Budapest
-                                (Magyarország)
+            <Timeline>
+                {sumScoolData.map((item, i) => (
+                    <TimelineItem
+                        key={`school-timeline-item-${i}`}
+                        oppContent={
+                            <Typography color='textSecondary'>
+                                {item.year}
                             </Typography>
-                            <StaticRecursiveTreeView data={gdfSchoolData} />
-                        </Box>
-                    }
-                />
-                <ScoolTimelineItem
-                    year={'2014 - 2019'}
-                    content={
-                        <Box>
-                            <Typography>
-                                Pázmány Péter Katolikus Egyetem - Információs
-                                Technológiai Kar, Budapest (Magyarország)
-                            </Typography>
-                            <StaticRecursiveTreeView data={pazmanySchoolData} />
-                        </Box>
-                    }
-                />
-                <ScoolTimelineItem
-                    year={'2011 - 2014'}
-                    content={
-                        <Box>
-                            <Typography>
-                                Wattay Középiskola és Szakiskola, Kiskőrös
-                                (Magyarország)
-                            </Typography>
-                            <StaticRecursiveTreeView data={wattaySchoolData} />
-                        </Box>
-                    }
-                />
-                <ScoolTimelineItem
-                    year={'2008 - 2011'}
-                    content={
-                        <Box>
-                            <Typography>
-                                Kiskőrösi Petőfi Sándor Evangélikus Óvoda,
-                                Általános Iskola, Gimnázium és Kertészeti
-                                Szakközépiskola, Kiskőrös
-                            </Typography>
-                            <StaticRecursiveTreeView data={petofiSchoolData} />
-                        </Box>
-                    }
-                    last
-                />
+                        }
+                        content={
+                            <Box>
+                                <Typography>{item.name}</Typography>
+                                <StaticRecursiveTreeView data={item.data} />
+                            </Box>
+                        }
+                    />
+                ))}
             </Timeline>
         </Section>
     );
---
+++ src/Timeline.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import TimelineOriginal from '@material-ui/lab/Timeline';
+import TimelineItemOriginal from '@material-ui/lab/TimelineItem';
+import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
+import TimelineConnector from '@material-ui/lab/TimelineConnector';
+import TimelineContent from '@material-ui/lab/TimelineContent';
+import TimelineDot from '@material-ui/lab/TimelineDot';
+import TimelineOppositeContent from '@material-ui/lab/TimelineOppositeContent';
+import { createStyles, makeStyles } from '@material-ui/core';
+
+export const useStyles = makeStyles((theme) =>
+    createStyles({
+        timelineRoot: {
+            margin: 0,
+            padding: 0,
+        },
+        topc: {
+            maxWidth: '100px',
+            paddingLeft: 0,
+        },
+    }),
+);
+
+export function TimelineItem({ oppContent, content, last = false }) {
+    const classes = useStyles();
+
+    return (
+        <TimelineItemOriginal>
+            <TimelineOppositeContent className={classes.topc}>
+                {oppContent}
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+                <TimelineDot />
+                {!last && <TimelineConnector />}
+            </TimelineSeparator>
+            <TimelineContent>{content}</TimelineContent>
+        </TimelineItemOriginal>
+    );
+}
+
+export function Timeline({ children, ...props }) {
+    const classes = useStyles();
+
+    return (
+        <TimelineOriginal className={classes.timelineRoot} {...props}>
+            {children}
+        </TimelineOriginal>
+    );
+}
