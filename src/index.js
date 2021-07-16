87e82d2beb7ab7378a2192571f000f4aa21d72a5
danikova <kovacs.daniel.9509@gmail.com>
2021-07-16 16:40:15 +0200

    bg color

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CV from './CV';
+import './index.css';
+
+import Moment from 'react-moment';
+import 'moment/locale/hu';
+
+Moment.globalLocale = 'hu';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <CV />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <CV />
+    </React.StrictMode>,
+    document.getElementById('root'),
 );
