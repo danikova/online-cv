79e399de039dc604ec6756f3c0d2a6b9c3101763
danikova <kovacs.daniel.9509@gmail.com>
2021-07-21 15:20:42 +0200

    lang section initial

--- src/Sections/LanguagesSection.js
+++ src/Sections/LanguagesSection.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import Section from './Section';
 import PublicIcon from '@material-ui/icons/Public';
+import { createStyles, Grid, makeStyles, Typography } from '@material-ui/core';
 
+export const useStyles = makeStyles((theme) =>
+    createStyles({
+        rightAlignedGrid: {
+            display: 'flex',
+            justifyContent: 'flex-end',
+        },
+    }),
+);
 
 export function LanguagesSection() {
+    const classes = useStyles();
+
     return (
         <Section icon={PublicIcon} title='Nyelvi ismeretek'>
-            <div>asd</div>
+            <Grid container spacing={3}>
+                <Grid item xs={2} className={classes.rightAlignedGrid}>
+                    <Typography>Angol</Typography>
+                </Grid>
+                <Grid item xs={10}>
+                    <Typography variant={'caption'}>
+                        társalgasi szint, szakmai nyelv
+                    </Typography>
+                </Grid>
+                <Grid item xs={2} className={classes.rightAlignedGrid}>
+                    <Typography>Német</Typography>
+                </Grid>
+                <Grid item xs={10}>
+                    <Typography variant={'caption'}>
+                        társalgasi szint (régen nem használtam)
+                    </Typography>
+                </Grid>
+            </Grid>
         </Section>
     );
 }
