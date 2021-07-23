ab95f2e03c9ddac2897a46a652934395cbbc12f3
danikova <kovacs.daniel.9509@gmail.com>
2021-07-23 12:12:50 +0200

    other sections init

--- src/Sections/HobbySection.js
+++ src/Sections/HobbySection.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import Section from './Section';
 import DirectionsBikeIcon from '@material-ui/icons/DirectionsBike';
-
+import { List, ListItem, ListItemText } from '@material-ui/core';
 
 export function HobbySection() {
     return (
         <Section icon={DirectionsBikeIcon} title='Hobby, szabadidő'>
-            <div>asd</div>
+            <List>
+                <ListItem>
+                    <ListItemText
+                        primary='programozok'
+                        secondary='legyen szó 3d-s dolgokról vagy saját játék fejlesztéséről esetleg saját weboldalakról'
+                    />
+                </ListItem>
+                <ListItem>
+                    <ListItemText
+                        primary='biciklizek'
+                        secondary='aktivan kerékpárral közlekedek a városban, szeretek hosszabb távokat tekerni'
+                    />
+                </ListItem>
+                <ListItem>
+                    <ListItemText
+                        primary='kerékpár szerelés'
+                        secondary='a nulláról építettem már több biciklit is'
+                    />
+                </ListItem>
+            </List>
         </Section>
     );
 }
