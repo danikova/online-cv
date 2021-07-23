2e46f10403b3bc548b67b1cd90e04a6724a33cae
danikova <kovacs.daniel.9509@gmail.com>
2021-07-23 12:13:01 +0200

    set theme

--- src/CV.js
+++ src/CV.js
@@ -8,21 +8,17 @@ import { Sections } from './Sections';
 const theme = createTheme({
     palette: {
         primary: {
-            light: '#757ce8',
-            main: '#3f50b5',
-            dark: '#002884',
-            contrastText: '#fff',
+            main: '#5584b1',
+            contrastText: '#fff'
         },
         secondary: {
-            light: '#ff7961',
-            main: '#f44336',
-            dark: '#ba000d',
-            contrastText: '#000',
+            main: '#f8f3ea',
+            contrastText: '#303030'
         },
     },
 });
 
-const useStyles = makeStyles((theme) =>
+const useStyles = makeStyles(() =>
     createStyles({
         cvPaper: {
             margin: '0.25in',
