a9b31d063af2f7d4e44ef16444e83f0fcaf6ab49
danikova <kovacs.daniel.9509@gmail.com>
2022-02-14 11:31:21 +0100

    GDF graduation update

--- src/Sections/SchoolSection.js
+++ src/Sections/SchoolSection.js
@@ -21,7 +21,7 @@ export const useStyles = makeStyles((theme) =>
 
 const sumScoolData = [
     {
-        year: '2019 - ',
+        year: '2019 - 2022',
         name: 'school.gdf.name',
     },
     {
