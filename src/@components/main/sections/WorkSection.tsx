2876ceeb8dd42e8fe3718f13085dd38c57db1989
danikova <kovacs.daniel.9509@gmail.com>
2024-08-26 15:01:07 +0200

    work section rework

--- src/@components/main/sections/WorkSection.tsx
+++ src/@components/main/sections/WorkSection.tsx
@@ -1,57 +1,65 @@
-import BaseSection from './BaseSection';
-import { MdWork } from 'react-icons/md';
-import humanizeDuration from 'humanize-duration';
-import { useLocale } from '@lang';
-import { FormattedMessage, useIntl } from 'react-intl';
-import Timeline, { TimelineItem } from '@components/Timeline';
-import { useDayJs } from '@utils/hooks';
-import { useMemo } from 'react';
+import BaseSection from "./BaseSection";
+import { MdWork } from "react-icons/md";
+import humanizeDuration from "humanize-duration";
+import { useLocale } from "@lang";
+import { FormattedMessage, useIntl } from "react-intl";
+import Timeline, { TimelineItem } from "@components/Timeline";
+import { useDayJs } from "@utils/hooks";
+import { useMemo } from "react";
+import dayjs from "dayjs";
 
 const sumWorkData = [
   {
-    company: 'work.nokia',
+    company: "work.nokia",
     timeline: [
       {
-        from: '2022.06.01',
+        from: "2024.01.01",
         to: new Date(),
         present: true,
-        title: 'work.nokiaWebGUIDev.title',
-        caption: 'work.nokiaWebGUIDev.caption',
+        title: "work.nokiaWebGUILeadDev.title",
+        caption: "work.nokiaWebGUILeadDev.caption",
       },
       {
-        from: '2020.01.01',
-        to: '2022.06.01',
-        title: 'work.nokiaSWSpecialist.title',
-        caption: 'work.nokiaSWSpecialist.caption',
+        from: "2022.06.01",
+        to: "2024.01.01",
+        present: true,
+        title: "work.nokiaWebGUIDev.title",
+        caption: "work.nokiaWebGUIDev.caption",
       },
       {
-        from: '2019.07.01',
-        to: '2020.01.01',
-        title: 'work.nokiaVerificationSpecialist.title',
-        caption: 'work.nokiaVerificationSpecialist.caption',
+        from: "2020.01.01",
+        to: "2022.06.01",
+        title: "work.nokiaSWSpecialist.title",
+        caption: "work.nokiaSWSpecialist.caption",
       },
       {
-        from: '2018.04.01',
-        to: '2019.07.01',
-        title: 'work.nokiaDevOpsEngineer.title',
-        caption: 'work.nokiaDevOpsEngineer.caption',
+        from: "2019.07.01",
+        to: "2020.01.01",
+        title: "work.nokiaVerificationSpecialist.title",
+        caption: "work.nokiaVerificationSpecialist.caption",
       },
       {
-        from: '2017.04.01',
-        to: '2018.04.01',
-        title: 'work.nokiaSWDeveloperIntern.title',
-        caption: 'work.nokiaSWDeveloperIntern.caption',
+        from: "2018.04.01",
+        to: "2019.07.01",
+        title: "work.nokiaDevOpsEngineer.title",
+        caption: "work.nokiaDevOpsEngineer.caption",
+      },
+      {
+        from: "2017.04.01",
+        to: "2018.04.01",
+        title: "work.nokiaSWDeveloperIntern.title",
+        caption: "work.nokiaSWDeveloperIntern.caption",
       },
     ],
   },
   {
-    company: 'work.cadline',
+    company: "work.cadline",
     timeline: [
       {
-        from: '2016.06.01',
-        to: '2016.08.01',
-        title: 'work.cadlineSWDeveloperIntern.title',
-        caption: 'work.cadlineSWDeveloperIntern.caption',
+        from: "2016.06.01",
+        to: "2016.08.01",
+        title: "work.cadlineSWDeveloperIntern.title",
+        caption: "work.cadlineSWDeveloperIntern.caption",
       },
     ],
   },
@@ -66,16 +74,24 @@ function WorkYears({ from, to, present = false }) {
   const fromStr = useDayJs(fromDate, "YYYY MMM");
   const toStr = useDayJs(toDate, "YYYY MMM");
 
-  // @ts-ignore
-  const durationText = useMemo(() => humanizeDuration(toDate - fromDate, {
-    language: locale,
-    units: ['y', 'mo'],
-    round: true,
-  }), [fromDate, toDate])
+  const durationText = useMemo(
+    () =>
+      // @ts-ignore
+      humanizeDuration(toDate - fromDate, {
+        language: locale,
+        units: ["y", "mo"],
+        round: true,
+      }),
+    [fromDate, toDate]
+  );
 
   return (
-    <div className='text-slate-500'>
-      <p>{fromStr}<span className="px-1">-</span>{present ? '' : toStr}</p>
+    <div className="text-slate-500">
+      <p>
+        {fromStr}
+        <span className="px-1">-</span>
+        {present ? "" : toStr}
+      </p>
       <p>{durationText}</p>
     </div>
   );
@@ -87,31 +103,60 @@ export function WorkSection() {
   return (
     <BaseSection
       icon={MdWork}
-      title={intl.formatMessage({ id: 'work.sectionTitle' })}
+      title={intl.formatMessage({ id: "work.sectionTitle" })}
     >
-      {sumWorkData.map((item, i) => (
-        <div key={`company-list-${i}`}>
-          <h4 className='text-lg mb-4 font-bold'>
-            <FormattedMessage id={item.company} />
-          </h4>
-          <Timeline>
-            {item.timeline.map((timelineData, j) => (
-              <TimelineItem
-                key={`timeline-item-${j}`}
-                oppContent={<WorkYears from={timelineData.from} to={timelineData.to} present={timelineData.present} />}
-                last={item.timeline.length - 1 === j}
-              >
-                <h5 className='font-bold'>
-                  <FormattedMessage id={timelineData.title} />
-                </h5>
-                <div className={`${item.timeline.length - 1 === j ? '' : 'mb-4'}`}>
-                  <FormattedMessage id={timelineData.caption} />
-                </div>
-              </TimelineItem>
-            ))}
-          </Timeline>
-        </div>
-      ))}
+      {sumWorkData.map((item, i) => {
+        const sumWorkTimeInMonths = item.timeline.reduce((acc, curr) => {
+          const fromDate = dayjs(curr.from);
+          const toDate = curr.present ? dayjs() : dayjs(curr.to);
+          return acc + toDate.diff(fromDate, "month");
+        }, 0);
+        const inYears = Math.round(sumWorkTimeInMonths / 12);
+
+        return (
+          <div key={`company-list-${i}`}>
+            <h4 className="text-lg mb-4 font-bold">
+              <FormattedMessage id={item.company} />{" "}
+              {inYears >= 1 && (
+                <span className="opacity-50">
+                  (
+                  <FormattedMessage
+                    id="work.sumYearsLabel"
+                    values={{ count: inYears }}
+                  />
+                  )
+                </span>
+              )}
+            </h4>
+            <Timeline>
+              {item.timeline.map((timelineData, j) => (
+                <TimelineItem
+                  key={`timeline-item-${j}`}
+                  oppContent={
+                    <WorkYears
+                      from={timelineData.from}
+                      to={timelineData.to}
+                      present={timelineData.present}
+                    />
+                  }
+                  last={item.timeline.length - 1 === j}
+                >
+                  <h5 className="font-bold">
+                    <FormattedMessage id={timelineData.title} />
+                  </h5>
+                  <div
+                    className={`${
+                      item.timeline.length - 1 === j ? "" : "mb-4"
+                    }`}
+                  >
+                    <FormattedMessage id={timelineData.caption} />
+                  </div>
+                </TimelineItem>
+              ))}
+            </Timeline>
+          </div>
+        );
+      })}
     </BaseSection>
   );
 }
